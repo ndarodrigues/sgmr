031b16521d7088a081f5ea1d55436d02da32409d
aaguiarpontualca <andre.aguiar@pontualca.com.br>
2025-06-09 16:24:47 -0300

    Ajustes

--- webapp/controller/ObjectPageSection.controller.js
+++ webapp/controller/ObjectPageSection.controller.js
@@ -3,9 +3,11 @@ sap.ui.define([
     "com/pontual/sgmr/model/formatter",
     'sap/m/MessagePopover',
     'sap/m/MessageItem',
-    'sap/ui/model/json/JSONModel'
+    'sap/ui/model/json/JSONModel',
+    "sap/m/Dialog",
+    "sap/m/Button",
 ],
-    function (Controller, formatter, MessagePopover, MessageItem, JSONModel) {
+    function (Controller, formatter, MessagePopover, MessageItem, JSONModel, Dialog, Button) {
         "use strict";
         var oView
         var oController
@@ -15,7 +17,7 @@ sap.ui.define([
                 oController = this;
                 oController.oController = this;
                 oView = oController.getView();
-                
+
                 var oModel = new JSONModel();
                 oModel.setData([]);
                 this.getView().setModel(oModel);
@@ -36,8 +38,43 @@ sap.ui.define([
 
             _handleRouteMatched: function (oEvent) {
 
-                
-            }
+
+            },
+
+            _handleExibirRoleteSuperior: function (oEvent) {
+                oController._handleExibirImagem()
+            },
+
+            _handleExibirImagem: function (oEvent) {
+                // Create the image
+                var oImage = new sap.m.Image({
+                    src: "http://localhost:8080/img/roletesuperior.png",
+                    width: "100%",    // Adjust width and height as needed
+                    height: "auto",
+                    decorative: false, // For accessibility
+                    alt: "Rolete Superior"
+                });
+
+                // Create the dialog
+                var oDialog = new sap.m.Dialog({
+                    title: "Rolete Superior",
+                    content: [oImage],
+                    beginButton: new sap.m.Button({
+                        text: "Fechar",
+                        press: function () {
+                            oDialog.close();
+                        }
+                    }),
+                    afterClose: function () {
+                        oDialog.destroy(); // Optional: Clean up
+                    }
+                });
+
+                // Open the dialog
+                oDialog.open();
+
+
+            },
 
         });
     });
