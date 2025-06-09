491bc6fda1ecc972826138f33f5ddbb8b755ec60
aaguiarpontualca <andre.aguiar@pontualca.com.br>
2025-06-09 18:02:48 -0300

    Ajustes inspecao

--- webapp/view/SharedBlocks/Inspecao.js
+++ webapp/view/SharedBlocks/Inspecao.js
@@ -2,15 +2,15 @@ sap.ui.define(['sap/uxap/BlockBase'],
 	function (BlockBase) {
 		"use strict";
 
-		var BlockBlueT1 = BlockBase.extend("com.pontual.sgmr.view.SharedBlocks.CondicaoOperacao", {
+		var BlockBlueT1 = BlockBase.extend("com.pontual.sgmr.view.SharedBlocks.Inspecao", {
 			metadata: {
 				views: {
 					Collapsed: {
-						viewName: "com.pontual.sgmr.view.SharedBlocks.CondicaoOperacao",
+						viewName: "com.pontual.sgmr.view.SharedBlocks.Inspecao",
 						type: "XML"
 					},
 					Expanded: {
-						viewName: "com.pontual.sgmr.view.SharedBlocks.CondicaoOperacao",
+						viewName: "com.pontual.sgmr.view.SharedBlocks.Inspecao",
 						type: "XML"
 					}
 				}
