60ef7fdff0a00ff6755a812726e09a7860401aa1
ndarodrigues <nadia.cazuze@pontualca.com.br>
2025-06-10 11:18:21 -0300

    Roletes Inferiores

--- webapp/view/SharedBlocks/RoletesInferiores.js
+++ webapp/view/SharedBlocks/RoletesInferiores.js
@@ -2,21 +2,21 @@ sap.ui.define(['sap/uxap/BlockBase'],
 	function (BlockBase) {
 		"use strict";
 
-		var RoletesSuperiores = BlockBase.extend("com.pontual.sgmr.controller.RoletesInferiores", {
+		var BlockBlueT2 = BlockBase.extend("com.pontual.sgmr.view.SharedBlocks.RoletesInferiores", {
 			metadata: {
 				views: {
 					Collapsed: {
-						viewName: "com.pontual.sgmr.view.RoletesInferiores",
+						viewName: "com.pontual.sgmr.view.SharedBlocks.RoletesInferiores",
 						type: "XML"
 					},
 					Expanded: {
-						viewName: "com.pontual.sgmr.view.RoletesInferiores",
+						viewName: "com.pontual.sgmr.view.SharedBlocks.RoletesInferiores",
 						type: "XML"
 					}
 				}
 			}
 		});
 
-		return RoletesSuperiores;
+		return BlockBlueT2;
 
 	});
